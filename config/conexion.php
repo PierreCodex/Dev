6c2f92b2432fa7813f11d4f38641764abc74786e
DevJean7w7 <cordovanoexiste@gmail.com>
2023-03-15 15:02:07 -0500

    15/03/23

--- config/conexion.php
+++ config/conexion.php
@@ -3,7 +3,7 @@
         protected $dbh;
         protected function conexion(){
             try{
-                $conectar= $this->dbh = new PDO("mysql:local=localhost;dbname=paginawebphp","root","");
+                $conectar= $this->dbh = new PDO("mysql:host=us-cdbr-east-06.cleardb.net;dbname=heroku_4f7cc8515460237","b93cea35321455","5c78a638");
                 return $conectar;
             }catch(Exception $e){
                 print "Error :".$e->getMessage();
